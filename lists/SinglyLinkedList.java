6ee7bdfda1306581cd9539539daf9eada6826a62
piexie3 <emmanuelkbett3@gmail.com>
2025-01-02 13:06:20 +0300

    delete functionalities

--- lists/SinglyLinkedList.java
+++ lists/SinglyLinkedList.java
@@ -26,6 +26,8 @@ public class SinglyLinkedList {
         sll.insert(7, 6);
         sll.insert(1, 5);
         sll.printLinkedList();
+        System.out.println(sll.deleteFirstNode().data);
+        sll.printLinkedList();
     }
 
     // singly linked list
@@ -102,4 +104,107 @@ public class SinglyLinkedList {
             node.next = currentListNode;
         }
     }
+
+
+    // deleting first Node of a sll
+    public ListNode deleteFirstNode(){
+        if (head == null) {
+            return null;
+        }
+        ListNode temp = head;
+        head = head.next;
+        temp.next = null;
+        return temp;
+    }
+
+    // deleting last node in a sll
+    public ListNode deleteLastNode(){
+        if (head==null || head.next==null) {
+            return head;
+        }
+        ListNode current = head;
+        ListNode previus = null;
+        while (current.next!=null) {
+            previus= current;
+            current = current.next;
+        }
+        previus.next = null;
+        return current;
+
+    }
+
+    /*
+     * Q. Implement method to delete a node at a given position. Assuming position
+     * to be valid and starting from 1
+     */
+    public void deleteNodeInAPosition(int position){
+        if (position==1) {
+            head = head.next;
+        }else{
+            ListNode previous = head;
+            int count = 0;
+            while (count<position-1) {
+                previous = previous.next;
+                count++;
+            }
+            ListNode current = previous.next;
+            previous.next = current.next;
+        }
+    }
+
+    // searching an element in a linked list
+    public boolean isValuPresent(int searchKey){
+        ListNode current = head;
+        while (current != null) {
+            if (current.data == searchKey) {
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
+    }
+
+
+    // reversing sll
+    public ListNode reverseSll(){
+        ListNode current = head;
+        ListNode previous = null;
+        ListNode next= null;
+        while (current!= null) {
+            next = current.next;
+            current.next = previous;
+            previous = next;
+            current = next;
+        }
+        return previous;
+    }
+
+    // finding the nth node from the end of the linked list
+    public ListNode getNthValueFromEnd(int n){
+        if (head == null) {
+            return null;
+        }
+        if (n<= 0) {
+            throw new IllegalArgumentException("Invalid value of n");
+        }
+        ListNode mainPtr = head;
+        ListNode refPtr = head;
+        int count = 0;
+
+        while (count<n) {
+            if (refPtr == null) {
+                throw new IllegalArgumentException(n+" is greather than number of nodes in the list");
+            }
+            refPtr = refPtr.next;
+            count++;
+            
+        }
+        while (refPtr!= null) {
+            refPtr = refPtr.next;
+            mainPtr = mainPtr.next;
+        }
+        return mainPtr;
+    }
+
+    // how to remove duplicates from sorted linked list
 }
